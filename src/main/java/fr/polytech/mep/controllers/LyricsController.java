28895b7895410e44db2080ba47073da4fe3e3754
Negikami <ax.jacque@hotmail.fr>
2022-12-02 23:06:53 +0100

    rr removed

--- src/main/java/fr/polytech/mep/controllers/LyricsController.java
+++ src/main/java/fr/polytech/mep/controllers/LyricsController.java
@@ -15,16 +15,15 @@ import fr.polytech.mep.repositories.LyricsRepository;
 class LyricsController {
     
 	@Autowired
-    private LyricsRepository centerRep;
+    private LyricsRepository lyricsRep;
 
     LyricsController() {
     }
 
     @GetMapping("/line")
     String getLine(@RequestParam(name="id",defaultValue = "0")long id) {
-        InitDb();
         try{
-            Lyrics l = centerRep.findById(id).get();
+            Lyrics l = lyricsRep.findById(id).get();
             return l.getLyric();
         }
         catch(NoSuchElementException e){
@@ -34,74 +33,13 @@ class LyricsController {
 
     @GetMapping("/")
     String random() {
-        long nombreAleatoire = (long)(Math.random() * (centerRep.count()));
+        long nombreAleatoire = (long)(Math.random() * (lyricsRep.count()));
         return getLine(nombreAleatoire);
     }
 
-    @GetMapping("/test")
+    @GetMapping("/ping")
     String test() {
-        return "Hello";
+        return "Pong !";
     }
 
-    public void InitDb(){
-		if(centerRep.count()>0)
-			return;
-		String[] l = {"We're no strangers to love",
-		"You know the rules and so do I (do I)",
-		"A full commitment's what I'm thinking of",
-		"You wouldn't get this from any other guy",
-		"I just wanna tell you how I'm feeling",
-		"Gotta make you understand",
-		"Never gonna give you up",
-		"Never gonna let you down",
-		"Never gonna run around and desert you",
-		"Never gonna make you cry",
-		"Never gonna say goodbye",
-		"Never gonna tell a lie and hurt you",
-		"We've known each other for so long",
-		"Your heart's been aching, but you're too shy to say it (say it)",
-		"Inside, we both know what's been going on (going on)",
-		"We know the game and we're gonna play it",
-		"And if you ask me how I'm feeling",
-		"Don't tell me you're too blind to see",
-		"Never gonna give you up",
-		"Never gonna let you down",
-		"Never gonna run around and desert you",
-		"Never gonna make you cry",
-		"Never gonna say goodbye",
-		"Never gonna tell a lie and hurt you",
-		"Never gonna give you up",
-		"Never gonna let you down",
-		"Never gonna run around and desert you",
-		"Never gonna make you cry",
-		"Never gonna say goodbye",
-		"Never gonna tell a lie and hurt you",
-		"We've known each other for so long",
-		"Your heart's been aching, but you're too shy to say it (to say it)",
-		"Inside, we both know what's been going on (going on)",
-		"We know the game and we're gonna play it",
-		"I just wanna tell you how I'm feeling",
-		"Gotta make you understand",
-		"Never gonna give you up",
-		"Never gonna let you down",
-		"Never gonna run around and desert you",
-		"Never gonna make you cry",
-		"Never gonna say goodbye",
-		"Never gonna tell a lie and hurt you",
-		"Never gonna give you up",
-		"Never gonna let you down",
-		"Never gonna run around and desert you",
-		"Never gonna make you cry",
-		"Never gonna say goodbye",
-		"Never gonna tell a lie and hurt you",
-		"Never gonna give you up",
-		"Never gonna let you down",
-		"Never gonna run around and desert you",
-		"Never gonna make you cry",
-		"Never gonna say goodbye",
-		"Never gonna tell a lie and hurt you"};
-        for (String string : l) {
-            centerRep.save(new Lyrics(string));
-        }
-	}
 }
