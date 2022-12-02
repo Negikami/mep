da4734c8e0662e75a22f6f3be108794dbd23cc80
Negikami <ax.jacque@hotmail.fr>
2022-12-02 20:40:14 +0100

    link to db

---
+++ src/main/java/fr/polytech/mep/controllers/LyricsController.java
@@ -0,0 +1,18 @@
+package fr.polytech.mep.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class LyricsController {
+    
+    LyricsController() {
+
+    }
+
+    @GetMapping("/")
+    String all(@RequestParam(name="id",defaultValue = "0")long id) {
+        return String.valueOf(id);
+    }
+}
---
+++ src/main/java/fr/polytech/mep/models/Lyrics.java
@@ -0,0 +1,6 @@
+package fr.polytech.mep.models;
+
+public class Lyrics {
+    private long id;
+    private String lyric;
+}
