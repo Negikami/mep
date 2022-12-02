40bd1ca3d04cb55b8b4c6430ad773ca8b2988f63
Negikami <ax.jacque@hotmail.fr>
2022-12-02 21:32:26 +0100

    buteuh

--- src/main/java/fr/polytech/mep/MepApplication.java
+++ src/main/java/fr/polytech/mep/MepApplication.java
@@ -2,8 +2,9 @@ package fr.polytech.mep;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class MepApplication {
 
 	public static void main(String[] args) {
